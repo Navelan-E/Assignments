770640361fd124f23a30716444014f1059ab87c4
Navelan-E <navelan.e@rently.com>
2025-11-13 12:49:18 +0530

    Makefile static and dynamic linking work

---
+++ files/my_lib.c
@@ -0,0 +1,15 @@
+#include "my_lib.h"
+int add(int a,int b){
+    return a+b;
+}
+int sub(int a,int b){
+    return a-b;
+}
+int multiply(int a,int b){
+    return a*b;
+}
+int divide(int a,int b){
+    if (b==0)
+        return 0;
+    return a/b;
+}
